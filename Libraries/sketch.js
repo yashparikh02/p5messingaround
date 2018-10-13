3b7072a77e826f2597e3a1c8e6591d24da01842c
Yash Parikh <yashparikh02@yahoo.com>
2018-10-13 15:56:06 -0400

    testing

--- Libraries/sketch.js
+++ Libraries/sketch.js
@@ -1,85 +1,60 @@
 
-var colors = [];
+var xspacing = 10;    // Distance between each horizontal location
+var w;                // Width of entire wave
+var theta = 0.0;      // Start angle at 0
+var amplitude; // Height of wave
+var period = 500.0;   // How many pixels before the wave repeats
+var dx;               // Value for incrementing x
+var yvalues;  // Using an array to store height values for the wave
 
 function setup() {
-  createCanvas(1500,500);
-  noFill();
-  addColors();
-  smooth(8);
-  background(20);
-	noLoop();
+  createCanvas(710, 400);
+  w = width+16;
+  dx = (TWO_PI / period) * xspacing;
+  yvalues = new Array(floor(w/xspacing));
+  amplitude= Math.floor(Math.random() * ((2-1)+1) + 2);
+
+  setInterval(function() {
+           amplitude= Math.floor(Math.random() * ((2-1)+1) + 2);
+      }, 3000);
+r = random(255);
+g = random(255);
+b = random(255);
 }
-
-
-function addColors() {
-  var c;
-  c = color(0, 138, 176);
-  colors[0] = c;
-  c = color(241,100,93);
-  colors[1] = c;
-  c = color(0,176,133);
-  colors[2] = c;
-  c = color(233,108,31);
-  colors[3] = c;
-  c = color(241,114,172);
-  colors[4] = c;
-  c = color(222,57,108);
-  colors[5] = c;
-  c = color(231,206,0);
-  colors[6] = c;
-  c = color(72,22,108);
-  colors[7] = c;
-  c = color(44,164,74);
-  colors[8] = c;
-
-
-}
-
-function getRandomColor() {
-
-  var i = Math.floor(random(colors.length));
-  var c = colors[i];
-  return c;
-
-}
-
 
 function draw() {
-
-  makeRibbons();
-
+  background(0);
+  calcWave();
+  renderWave();
 }
 
+function calcWave() {
+  // Increment theta (try different values for
+  // 'angular velocity' here)
+  theta += 0.02;
 
 
-
-function makeRibbons() {
-
-  for (var i=0; i < random(10)+2; i++) {
-
-    var strokeW = random(5)+3;
-
-    var amount = 500;
-    var frequency = random(1.0)/15;
-    var offset = random(200)+5;
-
-    var col = getRandomColor();
-
-    strokeWeight(strokeW);
-    stroke(col,180);
-    var startY = height/2;
-    beginShape();
-      vertex(0, startY);
-       for (var c=1; c < amount; c++) {
-         var sinoffset = sin(frequency*c);
-         var sinX = c*(width/amount);
-         var sinY = startY + (sinoffset*offset);
-         bezierVertex(sinX,sinY,sinX,sinY-1,sinX,sinY);
-       }
-    endShape();
-
+  // For every x value, calculate a y value with sine function
+  var x = theta;
+  for (var i = 0; i < yvalues.length; i++) {
+    yvalues[i] = sin(x)*amplitude*100*sin(2*x)*sin(4*x)*cos(x/2)^2;
+    x+=dx;
   }
-
-
-
 }
+
+function renderWave() {
+  noStroke();
+  fill(r,g,b);
+
+  // A simple way to draw the wave with an ellipse at each location
+  for (var x = 0; x < yvalues.length; x++) {
+    ellipse(x*xspacing, height/2+yvalues[x], 6, 6);
+  }
+}
+
+function mousePressed() {
+    // Pick new random color values
+    r = random(255);
+    g = random(255);
+    b = random(255);
+  }
