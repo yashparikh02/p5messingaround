b5a6c93d93aae0a4e944aabd54cec382b0853169
Yash Parikh <yashparikh02@yahoo.com>
2018-10-27 19:12:36 -0400

    squaremotion

--- Libraries/sketch.js
+++ Libraries/sketch.js
@@ -1,60 +1,41 @@
-
-var xspacing = 10;    // Distance between each horizontal location
-var w;                // Width of entire wave
-var theta = 0.0;      // Start angle at 0
-var amplitude; // Height of wave
-var period = 500.0;   // How many pixels before the wave repeats
-var dx;               // Value for incrementing x
-var yvalues;  // Using an array to store height values for the wave
+var xoff=500 ;
+var yoff=100 ;
+var bugs = [] ;
 
 function setup() {
-  createCanvas(710, 400);
-  w = width+16;
-  dx = (TWO_PI / period) * xspacing;
-  yvalues = new Array(floor(w/xspacing));
-  amplitude= Math.floor(Math.random() * ((2-1)+1) + 2);
-
-  setInterval(function() {
-           amplitude= Math.floor(Math.random() * ((2-1)+1) + 2);
-      }, 3000);
-r = random(255);
-g = random(255);
-b = random(255);
+  createCanvas(windowWidth,windowHeight); // windowWidth,windowHeight);
+    for (var i=0; i<200; i++) {
+  bugs.push(new square());
+}
 }
 
 function draw() {
-  background(0);
-  calcWave();
-  renderWave();
+  background(100,70,60);
+
+for (var i=0; i<bugs.length; i++) {
+  bugs[i].display();
+  //rotate(radians(20));
 }
 
-function calcWave() {
-  // Increment theta (try different values for
-  // 'angular velocity' here)
-  theta += 0.02;
 
-
-  // For every x value, calculate a y value with sine function
-  var x = theta;
-  for (var i = 0; i < yvalues.length; i++) {
-    yvalues[i] = sin(x)*amplitude*100*sin(2*x)*sin(4*x)*cos(x/2)^2;
-    x+=dx;
-  }
 }
 
-function renderWave() {
-  noStroke();
-  fill(r,g,b);
+function square() {
+  var xoff = Math.random() * 1000;
+  var yoff = Math.random() * 1000;
+  var x = map(noise(xoff),0,0,0, width);
+  var y= map(noise(yoff), 0,0,0, height);
 
-  // A simple way to draw the wave with an ellipse at each location
-  for (var x = 0; x < yvalues.length; x++) {
-    ellipse(x*xspacing, height/2+yvalues[x], 6, 6);
-  }
-}
 
-function mousePressed() {
-    // Pick new random color values
-    r = random(255);
-    g = random(255);
-    b = random(255);
+    this.display = function() {
+      fill(255, 0, 0, 50);
+      noStroke()
+      rect(x,y, 20, 20);
+      yoff += Math.random()/100;
+      xoff -= Math.random()/100;
+      x = map(noise(xoff),0,1,0, width);
+      y = map(noise(yoff),0,1,0, height);
+
+
+    };
   }
